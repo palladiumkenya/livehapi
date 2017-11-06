43a89c79448ab1a85fa5b93248c0332b374be6ad
Koske Kimutai <koskedk@live.com>
2017-11-06 18:33:08 +0300

    v1.5

--- LiveHAPI/Startup.cs
+++ LiveHAPI/Startup.cs
@@ -21,11 +21,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace LiveHAPI
 {
     public class Startup
     {
+
         public static IConfiguration Configuration;
 
         public Startup(IHostingEnvironment env)
@@ -150,7 +152,8 @@ namespace LiveHAPI
 
             });
 
-        
+
+            Log.Debug($"im hAPI !!");
         }
     }
 }
