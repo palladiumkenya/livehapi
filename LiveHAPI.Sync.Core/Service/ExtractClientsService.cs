e3d1629907bde0e16c979a857eb3021eab7ce78a
Koske Kimutai <koskedk@live.com>
2018-05-24 10:52:23 +0300

    added

---
+++ LiveHAPI.Sync.Core/Service/ExtractClientsService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using LiveHAPI.Sync.Core.Interface.Extractors;
+using LiveHAPI.Sync.Core.Interface.Services;
+using LiveHAPI.Sync.Core.Interface.Writers;
+
+namespace LiveHAPI.Sync.Core.Service
+{
+    public class ExtractClientsService : IExtractClientsService
+    {
+        private readonly IClientStageExtractor _clientStageExtractor;
+        private readonly IClientStageRelationshipExtractor _clientStageRelationshipExtractor;
+        private readonly IClientPretestStageExtractor _clientPretestStageExtractor;
+        
+        
+        private readonly IIndexClientMessageWriter _clientMessageWriter;
+        private readonly IPartnerClientMessageWriter _partnerClientMessageWriter;
+        private readonly IFamilyClientMessageWriter _familyClientMessageWriter;
+
+        public ExtractClientsService(IIndexClientMessageWriter clientMessageWriter, IPartnerClientMessageWriter partnerClientMessageWriter, IFamilyClientMessageWriter familyClientMessageWriter)
+        {
+            _clientMessageWriter = clientMessageWriter;
+            _partnerClientMessageWriter = partnerClientMessageWriter;
+            _familyClientMessageWriter = familyClientMessageWriter;
+        }
+
+
+        public async Task<int> Sync()
+        {
+            
+            await _clientMessageWriter.Write();
+            
+            await _partnerClientMessageWriter.Write();
+            
+            await _familyClientMessageWriter.Write();
+            
+            return 1;
+        }
+    }
+}
