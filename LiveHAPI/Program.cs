157f43568da634822335c11612923c5f9481cef5
Koske Kimutai <koskedk@live.com>
2018-06-19 12:44:05 +0300

    v126

--- LiveHAPI/Program.cs
+++ LiveHAPI/Program.cs
@@ -51,7 +51,7 @@ namespace LiveHAPI
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .UseSerilog()
-                .UseUrls("http://0.0.0.0:4700") //TODO: Remove this setting
+                //.UseUrls("http://0.0.0.0:4700") //TODO: Remove this setting
                 .Build();
             return host;
         }
