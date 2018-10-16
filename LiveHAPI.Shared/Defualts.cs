10507c39a1b9a2581b8b7ebcff2739f45e6b0303
koske kimutai <koskedk@live.com>
2018-10-16 13:31:13 +0300

    hapi 105

--- LiveHAPI.Shared/Defualts.cs
+++ LiveHAPI.Shared/Defualts.cs
@@ -6,7 +6,7 @@ namespace LiveHAPI.Shared
 {
     public static class Defualts
     {
-        public static string SyncVersion = "104";
+        public static string SyncVersion = "105";
         public static string Environment = "";
     }
 }
